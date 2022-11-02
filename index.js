fb0271797a92625476254057a03c9bcce4587b55
julian camilo benavides vanegas <correoj94@gmail.com>
2022-11-02 18:29:15 -0500

    cors added

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-const express = require('express');
-const petAPI = require('./routes/petRoute');
-require('dotenv').config();
+const express = require("express");
+const petAPI = require("./routes/petRoute");
+require("dotenv").config();
 
 const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  let allowedOrigins = ["http://localhost:4200"];
+
+  let origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, token, authorization, apiKeyToken, responseType, content-type"
+  );
+
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+
+  next();
+});
+
 petAPI(app);
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Listening on ${process.env.HOST}:${process.env.PORT}/api`);
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on ${process.env.HOST}:${process.env.PORT}/api`);
+});
